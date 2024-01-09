a3d50b0a3a85a017fd0649c858e87956b49e11d4
Camila Pinto <146131971+PintoCamila@users.noreply.github.com>
2024-01-09 18:09:51 +0000

    added hackaton's entry

---
+++ hackaton/spa-api/js/app/controllers/menu-controller.js
@@ -0,0 +1,22 @@
+define(["views/menu-view", "router"], function (menuView, router) {
+  const externals = {};
+  const internals = {};
+
+  externals.start = function () {
+    menuView.render();
+    internals.createButton();
+  };
+
+  //PLAY BUTTON
+  internals.createButton = function () {
+    $("#goToGame").on("click", function () {
+      $("#menu").empty();
+      $(`#css6`).remove();
+      window.location.hash = "#game";
+    });
+  };
+
+  //vai buscar as categories com o trivia service
+
+  return externals;
+});
